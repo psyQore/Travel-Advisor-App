42a86c4f4df6f3b1f2a1bb255f5393fe59cbaa5e
Juan Galarce <j6alarce@gmail.com>
2022-01-21 10:51:47 -0300

    Se crea la estructura del mapa de google, se crea el proyecto para obtener api

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -19,18 +19,18 @@ const Header = () => {
             Explore new places
           </Typography>
           {/* <Autocomplete> */}
-            <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon />
-              </div>
-              <InputBase
-                placeholder='Search...'
-                classes={{
-                  root: classes.inputRoot,
-                  input: classes.inputInput,
-                }}
-              />
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
             </div>
+            <InputBase
+              placeholder='Search...'
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+            />
+          </div>
           {/* </Autocomplete> */}
         </Box>
       </Toolbar>
